c2bc7d520a6aff8b0a70df34344d408b133deb72
BrunoSan <Bruno-kk@hotmail.com>
2021-10-24 04:36:52 -0300

    mini ecommerce em Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductsUpdateComponent } from './components/product/products-update/products-update.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './views/home/home.component';
+import {ProductsCrudComponent} from './views/products-crud/products-crud.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path:"",
+  component: HomeComponent
+},
+  {
+    path:"products",
+    component:ProductsCrudComponent
+  },
+  {
+    path:"products/create",
+    component:ProductCreateComponent
+  },
+
+  {
+    path:"products/update/:id",
+    component: ProductsUpdateComponent
+  },
+
+  {
+    path:"products/delete/:id",
+    component:ProductDeleteComponent
+
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/product/product.service.ts
@@ -0,0 +1,46 @@
+import { Product } from './product.model';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar'
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  baseURL="http://localhost:3001/products";
+
+  constructor(private snackbar:MatSnackBar,private http:HttpClient) { }
+
+  showMessage(mg:string):void{
+    this.snackbar.open(mg,'X',{
+      duration:3000,
+      horizontalPosition:"right",
+      verticalPosition:"top"
+    })
+  }
+
+  create(product:Product):Observable<Product>{
+    return this.http.post<Product>(this.baseURL,product)
+  }
+
+  read():Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseURL)
+  }
+
+  readById(id:string):Observable<Product>{
+    const url =`${this.baseURL}/${id}`
+    return this.http.get<Product>(url)
+  }
+
+  update(product:Product):Observable<Product>{
+    const url =`${this.baseURL}/${product.id}`
+    return this.http.put<Product>(url,product)
+  }
+
+  delete(id:string):Observable<Product>{
+    const url =`${this.baseURL}/${id}`
+    return this.http.delete<Product>(url)
+  }
+}
